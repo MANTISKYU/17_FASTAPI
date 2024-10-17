0d01e73f3ce41705346af0c9fdd4775aa2c7757c
80418 <asdaron44@gmail.com>
2024-10-17 18:05:07 +0900

    todo

--- 08_todo/database.py
+++ 08_todo/database.py
@@ -12,7 +12,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 # sqlite
 # 파이썬에 내장되어있는 DB 라이브러리
-DB_URL = 'sqlite:///todo.sqlite3'
+DB_URL = "sqlite:///todo.sqlite3"
 
 # 데이터베이스 엔진 생성
 engine = create_engine(DB_URL)
@@ -20,7 +20,7 @@ engine = create_engine(DB_URL)
 # 연결 세션을 생성하기 위한 객체
 # autocommit : 자동 커밋을 비활성화
 # autoflush : 자동 플러시를 비활성화 (플러시 : 세션의 변경사항을 데이터베이스에 동기화)
-session_local = sessionmaker(autocommit=False , autoflush=False, bind=engine)
+session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # 모든 모델 클래스가 상속받을 기본 모델 클래스를 지정
 Base = declarative_base()
