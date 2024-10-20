57fe82f596cf0683c7cd36e29730f683f22c4cbb
MANTISKYU <asdaron44@gmail.com>
2024-10-20 23:31:07 +0900

    Update calculation.py

--- homework/calculation.py
+++ homework/calculation.py
@@ -11,3 +11,11 @@ def divide(a, b):
     if b == 0:
         raise ValueError("0으로는 나눌 수 없습니다")
     return a / b
+    
+def power(a, b):
+    return a ** b
+
+def square_root(a):
+    if a < 0:
+        raise ValueError("음수를 사용할 수 없습니다")
+    return a ** 0.5 
