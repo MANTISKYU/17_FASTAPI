4df8c6d233034f1d1e45f081accfe55c33963456
MANTISKYU <asdaron44@gmail.com>
2024-10-20 23:04:34 +0900

    Update main.py

---
+++ homework/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, HTTPException
+from calculation import plus, minus, multiply, divide
+
+app = FastAPI()
+
+@app.get("/calculate")
+async def calculate_api(calculation: str, a: float, b: float):
+    try:
+        if calculation == '+':
+            result = plus(a, b) 
+        elif calculation == '-':
+            result = minus(a, b) 
+        elif calculation == '*':
+            result = multiply(a, b)  
+        elif calculation == '/':
+            result = divide(a, b) 
+        else:
+            raise HTTPException(status_code=400, detail="Invalid calculation. Allowed operations are +, -, *, /.")
+        
+
+        return {f"{a} {calculation} {b}": result}
+    
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
