ff8e57a4b0efe87200c1f01adbff0bf1d7aace8f
MANTISKYU <asdaron44@gmail.com>
2024-10-20 22:54:21 +0900

    Update calculation.py

--- homework/calculation.py
+++ homework/calculation.py
@@ -1,1 +1,13 @@
+def plus(a, b):
+    return a + b
 
+def minus(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("0으로는 나눌 수 없습니다")
+    return a / b
